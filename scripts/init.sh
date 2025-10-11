fa4fd39f39504d0907b97ea6956f16c2e17b057e
JEYASHREESELVAN <144306786+JEYASHREESELVAN@users.noreply.github.com>
2025-10-11 15:38:26 +0530

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -11,11 +11,11 @@ fi
 rm -rf /workspaces/frappe_codespace/.git
 
 source /home/frappe/.nvm/nvm.sh
-nvm install 18
-nvm alias default 18
-nvm use 18
+nvm install 22
+nvm alias default 22
+nvm use 22
 
-echo "nvm use 18" >> ~/.bashrc
+echo "nvm use 22" >> ~/.bashrc
 cd /workspace
 
 npm install -g yarn
